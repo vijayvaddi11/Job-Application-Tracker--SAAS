3612dbc6af2bc5a0eb083e379b2cff8b1c3d8ded
vijayvaddi11 <vijayjay6688@gmail.com>
2026-02-12 22:36:16 +0530

    import dashboard component to App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,14 +2,29 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Register from "./components/Register.jsx";
 import Login from "./components/Login.jsx";
 import Profile from "./components/Profile.jsx";
+import Layout from "./components/Layout/Layout.jsx";
+import AddJob from "./components/AddJob.jsx";
+import EditJob from "./components/EditJob.jsx";
+import Dashboard from "./components/Dashboard.jsx";
+
+
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+         {/* Public routes with out Layout */}
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
+
+         {/* Private routes with Layout */}
+        <Route element={<Layout/>}>
         <Route path="/profile" element={<Profile />} />
+        <Route path='/addjob' element={<AddJob/>}/>
+        <Route path='/editjob' element={<EditJob/>}/>
+        <Route path='/dashboard' element={<Dashboard/>}/>
+        </Route>
+
       </Routes>
     </BrowserRouter>
   );
