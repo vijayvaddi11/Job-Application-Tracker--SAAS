0e1e2df9bad35de5ee9f330516d3ba54bb2f1c1c
vijayvaddi11 <vijayjay6688@gmail.com>
2026-02-13 15:39:00 +0530

    add job schema model for job applications

---
+++ backend/src/models/jobSchema.js
@@ -0,0 +1,53 @@
+import mongoose from "mongoose";
+const {Schema} = mongoose;
+
+const jobSchema = Schema({
+
+     userId:{
+          type: mongoose.Schema.Types.ObjectId,
+          ref:"User",
+          required :true
+
+     },
+
+     company:{
+          type: String,
+          required: true
+     },
+
+     jobTitle:{
+          type:String,
+          required:true
+     },
+
+     status:{
+          type:String,
+          enum:["Applied","Interview","Offer","Rejected"],
+          default:"Applied"
+     },
+
+     date:{
+          type: Date,
+          default: Date.now()
+     },
+
+     location:{
+          type:String,
+     },
+
+     salary:{
+          type:String,
+          default: "Not Mentioned"
+     },
+
+     notes:{
+          type:String,
+     }
+
+
+
+
+})
+
+
+export default jobSchema;
